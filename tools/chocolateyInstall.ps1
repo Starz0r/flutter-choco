51deb1ce0d85b27e6fcef8e4b8f4d51fa14fb89f
Starz0r <starz0r@starz0r.com>
2019-04-26 20:39:50 -0500

    Revert "Bump to version v0.1.8-dev"
    This reverts commit 5d226bf3911a4f1136865be80f9b8eb75bdb9bfb.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://storage.googleapis.com/flutter_infra/releases/dev/windows/flutter_windows_v0.1.8-dev.zip'
+$url        = 'https://storage.googleapis.com/flutter_infra/releases/beta/windows/flutter_windows_v0.1.5-beta.zip'
 $installDir = Join-Path $env:ChocolateyToolsLocation "flutter"
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum 'F6A1103A203E42C7CA8425064E2F40D47EB3C51A5DF5F655BA6602CDE6599D09A9AD2F0137CDBFBC880FBA116B45C8AA8ACDD6DDB356FA41C2BE7BD87CB8B952' -checksumType 'sha512'
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum '04EF2DDA9173BE8538636A471CB9F7E182DDA38B65A5AAD76553729BCEE7576D04B423B9C04384B64155D59A8C24D42F16184ABF989B4A2BD9B13B3770C0694C' -checksumType 'sha512'
 Install-ChocolateyPath $installDir
