bf83427c3d19ebb80f8c4af753e05ccd1341be5f
Starz0r <starz0r@starz0r.com>
2019-04-26 23:04:08 -0500

    Bump to version 0.11.10-dev

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://storage.googleapis.com/flutter_infra/releases/beta/windows/flutter_windows_v0.11.9-beta.zip'
+$url        = 'https://storage.googleapis.com/flutter_infra/releases/dev/windows/flutter_windows_v0.11.10-dev.zip'
 $installDir = Join-Path $env:ChocolateyToolsLocation "flutter"
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum 'DCD7FA245F67EB594FDD5C73D41879DB0366CCF819398C9E25CF9EAAF9E7B5AED4AED28087B51CC31C322D4C2E56D7279E8C34F44CA20A6203BFF700B22D2766' -checksumType 'sha512'
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum 'B69BF69CE25578473F616A2E7ED3B7FFE84BD65CF200DFE4E9464C893056B32D5838AFE71A7B91AE7903D67CC90A7884B273A014C5B0A1FE3CAE6547FEB7A67C' -checksumType 'sha512'
 Install-ChocolateyPath $installDir
