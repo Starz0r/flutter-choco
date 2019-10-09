8fe2a3830906addeffedd0cb8695a0dcfe505f90
Starz0r <starz0r@starz0r.com>
2019-10-09 13:08:36 -0500

    Bump to version 1.6.0-dev

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://storage.googleapis.com/flutter_infra/releases/beta/windows/flutter_windows_v1.6.3-beta.zip'
-$installDir = Join-Path $env:ChocolateyToolsLocation "flutter"
+$ToolsPath             = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$url                   = 'https://storage.googleapis.com/flutter_infra/releases/dev/windows/flutter_windows_v1.6.0-dev.zip'
+$installDir            = $env:ChocolateyToolsLocation "flutter"
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum 'File 'C:\Users\Starz0r\Desktop\Flutter Publishing\flutter_windows_v1.6.3-beta.zip' doesn't exist.' -checksumType 'sha512' -SpecificFolder 'flutter\'
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum '1FD9C016B39BFCD46B75945EA7A81655EA585638AADF1AA0329773A3D75651FC65E3895E2F5A538D7C0C6AC9E71B37E0AAD60749F1372F1D5B9A63B3EB8A8AB8' -checksumType 'sha512' -SpecificFolder 'flutter'
 Install-ChocolateyPath $installDir
