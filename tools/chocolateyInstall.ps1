b94ae0e36e57540f7896fdc6745a67f056eee2b5
Starz0r <starz0r@starz0r.com>
2020-03-02 19:45:08 -0600

    Bump to version 1.14.6-dev

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $ToolsPath             = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url                   = 'https://storage.googleapis.com/flutter_infra/releases/beta/windows/flutter_windows_v1.10.7-beta.zip'
+$url                   = 'https://storage.googleapis.com/flutter_infra/releases/dev/windows/flutter_windows_v1.14.6-dev.zip'
 $installDir            = $env:ChocolateyToolsLocation # Dangerous but appending -SpecificFolder to Install-ChocolateyZipPackage doesn't work
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum '6B28BBADA71AAB04B16699AEFF15CC11CC80D7603599D5F108607B6692AC282D7684350E8607B5B33B1A558EC778B2249CF540BDE245BA0FA7BEC3D34C4CF3AF' -checksumType 'sha512'
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir -checksum '8AD396087E557698C99B00C70273CCA77E746AC154BCE7F52C3FACEB8F7CF87101F280768F39D8C85E87F7384B857918F4D5A743267D072C81D8CECC496C1D7A' -checksumType 'sha512'
 Install-ChocolateyPath $installDir
